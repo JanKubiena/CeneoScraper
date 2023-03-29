68a148eb4c055698f36b357a103bd166cf036e7b
Jan <jan@kubiena.pl>
2023-03-29 14:39:02 +0200

    2023-03-29 JK

---
+++ scraper.py
@@ -0,0 +1,17 @@
+import requests
+from bs4 import BeautifulSoup
+# product_code = input("Podaj kod produktu: ")
+product_code = "26674875"
+url = f"https://www.ceneo.pl/{product_code}#tab=reviews"
+response = requests.get(url)
+page_dom = BeautifulSoup(response.text, 'html.parser')
+opinions = page_dom.select("div.js_product-review")
+for opinion in opinions:
+    print(opinion["data-entry-id"])
+
+print(type(page_dom))
+print(type(opinions))
+print(len(opinions))
+
+
+
